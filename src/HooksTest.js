bd01e0775e873c2db5aba06ad3efe2e572d8818b
cathjime <xcatherine.jimenez@gmail.com>
2021-01-25 11:06:03 -0500

    newState

--- src/HooksTest.js
+++ src/HooksTest.js
@@ -1,21 +1,32 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function HooksTest() {
-    let [ count, setCount ] = useState(0)
+  let [count, setCount] = useState(0);
+  let [num, setNum] = useState(1);
 
-    function increment(){
-        setCount(prevCount => prevCount + 1)
-    }
-    
-    function decrement(){
-        setCount(prevCount => prevCount - 1)
-    }
+  function increment() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function decrement() {
+    setCount((prevCount) => prevCount - 1);
+  }
+
+  function double() {
+    setNum((prevNum) => prevNum * 2);
+  }
+
+  function halves() {
+    setNum((prevNum) => prevNum / 2);
+  }
 
   return (
     <>
-      <h2>{count}</h2>
+      <h2>{num}</h2>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+      <button onClick={double}>Double</button>
+      <button onClick={halves}>Halves</button>
     </>
   );
 }
